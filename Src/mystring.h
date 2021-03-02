63978c1f2d2393c01f103586cf423978c3d595d0
rajath <rajathpd.pd@ltts.com>
2021-03-02 13:57:26 +0530

    Create mystring.h

---
+++ Src/mystring.h
@@ -0,0 +1,10 @@
+#include<string.h>
+#ifndef MYSTRING_H_
+#define MYSTRING_H_
+
+int strlen(const char *s);
+char* mystrcpy(char* dest, const char* src);
+char* mystrcat(char *destination, const char *source);
+int strcmp(const char *str1, const char *str2);
+
+#endif // MYSTRING_H_
