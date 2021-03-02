5efbd3707d531329d740ab19d57e8c86365d5ed4
rajath <rajathpd.pd@ltts.com>
2021-03-02 14:15:06 +0530

    Update mystring.c

--- Src/mystring.c
+++ Src/mystring.c
@@ -1,13 +1,13 @@
 #include "mystring.h"
-size_t strlen(const char *s){
+size_t mystrlen(const char *s){
     return strlen(s);
 }
-char* strcpy(char* dest, const char* src){
+char* mystrcpy(char* dest, const char* src){
     return strcpy(dest,src);
 }
-char* strcat(char *destination, const char *source){
+char* mystrcat(char *destination, const char *source){
     return strcat(destination,source);
 }
-int strcmp(const char *str1, const char *str2 ){
+int mystrcmp(const char *str1, const char *str2 ){
     return strcmp(str1,str2);
 }
