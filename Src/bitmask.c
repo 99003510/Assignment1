e8d08c445b43597802f763f97c3aa7eec655e752
rajath <rajathpd.pd@ltts.com>
2021-03-02 13:58:29 +0530

    Update bitmask.c

--- Src/bitmask.c
+++ Src/bitmask.c
@@ -1,17 +1,23 @@
 #include "bitmask.h"
+
 int set(int a, int r)
 {
     return (a | (1 << (r - 1)));
 }
 
-// Function to clear the rth bit of 'a'
+// clear the bit r
 int clear(int a, int r)
 {
     return (a & (~(1 << (r - 1))));
 }
 
-// Function to toggle the rth bit of 'a'
+//toggle
 int flip(int a, int r)
 {
     return (a ^ (1 << (r - 1)));
+}
+
+int query(int a, int r)
+{
+    return (a & (1 << (r-1)));
 }
