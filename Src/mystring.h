bc55dd69043fefd5ac77316a4b4fe09c0ac19eee
rajath <rajathpd.pd@ltts.com>
2021-03-02 14:11:09 +0530

    Update mystring.h

--- Src/mystring.h
+++ Src/mystring.h
@@ -2,9 +2,9 @@
 #ifndef MYSTRING_H_
 #define MYSTRING_H_
 
-int strlen(const char *s);
+size_t mystrlen(const char *s);
 char* mystrcpy(char* dest, const char* src);
 char* mystrcat(char *destination, const char *source);
-int strcmp(const char *str1, const char *str2);
+int mystrcmp(const char *str1, const char *str2);
 
 #endif // MYSTRING_H_
